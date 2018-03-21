327c9d361f48050218ebae51cfa688539faf132d
Yarden Livnat <yarden.livnat@gmail.com>
2018-03-20 21:47:15 -0600

    add new resampling code

---
+++ server/regulus_file.py
@@ -0,0 +1,135 @@
+import json
+import csv
+from pathlib import Path
+
+
+class RegulusFile(object):
+    def __init__(self, name = '', version = '1.0', dims = [], measures = [], pts = [], mscs = [], notes = ''):
+        self.name = name
+        self.dims = dims
+        self.version = version
+        self.measures = measures
+        self.pts = pts
+        self.mscs = mscs
+        self.notes = notes
+        self.new_sample_input = []
+        self.old_version = ''
+
+    def load_reg(self, data):
+        self.name = data['name']
+        self.dims = data['dims']
+        self.version = data['version']
+        self.measures = data['measures']
+        self.pts = data['pts']
+        self.mscs = data['mscs']
+        self.notes = data['notes']
+
+    def load_reg_json(self, filename = None, spec = None, version = None, name = None, dir = None):
+
+        if dir is not None:
+            cur_dir = dir
+        else:
+            cur_dir = Path('.')
+
+        if filename is not None:
+            self.dir = cur_dir
+            with open(cur_dir / filename) as json_data:
+                data = json.load(json_data)
+            self.cur_temp = data
+            self.load_reg(data)
+
+        elif spec is not None:
+            # spec is the file from
+            name = spec['name']
+            version = spec['version']
+            self.load_reg_json(name = name, version = version, dir = cur_dir)
+
+        else:
+
+            if (cur_dir / (name +'.'+ version + '.json')).exists():
+                self.load_reg_json(filename = name + '.'+ version + '.json', dir = cur_dir)
+
+            elif (cur_dir/(name +'.json')).exists():
+                self.load_reg_json(filename = name + '.json', dir= cur_dir)
+
+    def add_points(self, pts):
+        self.pts = self.pts + pts
+
+    def add_pts_from_csv(self, filename):
+        with open(filename, newline='') as csvfile:
+            reader = csv.reader(csvfile,delimiter=',')
+            header = next(reader)
+            data = [[float(x) for x in row] for row in reader]
+        self.add_points(data)
+
+    # Right now used to load specification received and update class
+    def update(self, spec = None, version = None, name = None, notes = None, new_version = None, new_input_object = None, new_sample = None):
+        if spec is not None:
+
+            self.name = spec['name']
+            self.old_version = self.version
+            self.version = spec['new_version']
+            self.extract_input(spec['pts'])
+
+        if version is not None:
+            self.version = version
+        if name is not None:
+            self.name = name
+        if notes is not None:
+            self.notes = notes
+        if new_input_object is not None:
+            self.extract_input(new_input_object)
+        if new_version is not None:
+            self.old_version = self.version
+            self.version = new_version
+
+        if new_sample is not None:
+            self.new_sample_input = new_sample
+
+
+    def extract_input(self, data):
+        self.new_sample_input = [[ptdict[inputdim] for inputdim in self.dims] for ptdict in data]
+
+    def save_sample_inputs(self, filename):
+
+        with open(filename, 'w') as f:
+            report = csv.writer(f,delimiter=',')
+            report.writerows(self.new_sample_input)
+
+    def report_sample_input(self):
+        return self.new_sample_input
+
+    def save_all_pts(self, dir = None):
+        if dir is None:
+            dir = self.dir
+        filename = str(dir/(self.name+'.'+self.version+'.csv'))
+        header = self.dims+self.measures
+        with open(filename, 'w') as f:
+            report = csv.writer(f,delimiter=',')
+            report.writerow(header)
+            report.writerows(self.pts)
+
+        return filename
+
+    def save_json(self, dir = None):
+
+        if dir is None:
+            dir = self.dir
+        reg_data = {}
+
+        reg_data['pts'] = self.pts
+        reg_data['mscs'] = self.mscs
+        reg_data['name'] = self.name
+        reg_data['version'] = self.version
+        reg_data['notes'] = self.notes
+        reg_data['measures'] = self.measures
+        reg_data['dims'] = self.dims
+
+        filename = str(dir/(self.name+'.'+self.version+'.json'))
+        with open(filename, 'w') as f:
+            json.dump(reg_data, f)
+
+        return filename
+
+
+
---
+++ server/sample.py
@@ -0,0 +1,52 @@
+import subprocess
+import threading
+import testfun
+from regulus_file import RegulusFile
+
+def sample_in_thread(reg_file):
+
+    sim_dir = 'temp'
+    sim_out = 'new_sample_outputs.csv'
+    sim_in = 'new_sample_inputs.csv'
+
+    reg_file.save_sample_inputs(sim_in)
+    sample_input = reg_file.report_sample_input()
+
+
+    if reg_file.name =='deployment':
+        subprocess.run(['python', '-m', 'scenario', '-t', 'Transition_scenario.xml', '-o', sim_dir, '-p',sim_in, '-r', sim_out], check=True)
+
+
+    if reg_file.name == 'test':
+        new_input = testfun.load_input(sample_input)
+        new_data = testfun.generateres(new_input)
+        testfun.savefile(new_data, sim_dir, sim_out)
+
+
+    reg_file.add_pts_from_csv(sim_dir+'/'+sim_out)
+    updated_dataset = reg_file.save_all_pts()
+    updated_json = reg_file.save_json()
+
+
+    if reg_file.name == 'test':
+        subprocess.run(['python', 'post.py', '-k', '50', '-d', '4', '--name', 'test', '--all', updated_dataset,'-j', '1', '-t',updated_json],check=True)
+
+    if reg_file.name == 'deployment':
+        subprocess.run(['python', 'post.py', '-k', '500', '-d', '6', '--name', 'deployment', '--all',  updated_dataset,'-j', '1', '-t',updated_json],check=True)
+
+    print("New Results are available")
+
+    return
+
+	
+def createsample(received, data_dir):
+    if received is not None:
+
+        reg_file = RegulusFile()
+        reg_file.load_reg_json(spec= received, dir = data_dir)
+        reg_file.update(spec = received)
+
+        thread = threading.Thread(target=sample_in_thread, args=[reg_file])
+        thread.start()
+
+    return
