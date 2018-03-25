e08d8d5240add820999746410a631f61aa209c69
Dan Maljovec <maljovec002@gmail.com>
2018-03-24 22:48:33 -0600

    Combining Di's changes with my own edits.

--- server/sample.py
+++ server/sample.py
@@ -1,17 +1,16 @@
 import subprocess
 import testfun
 from regulus_file import RegulusFile
+from linearfit import linear_fit
+
 from pathlib import Path
 from Predictor import Predictor
 import numpy as np
 import pandas as pd
 
-from linearfit import linear_fit
 sim_dir = 'temp'
 sim_out = 'new_sample_outputs.csv'
 sim_in = 'new_sample_inputs.csv'
-
-
 
 def sample(spec, data_dir):
     reg_file = RegulusFile()
@@ -28,15 +27,7 @@ def sample(spec, data_dir):
         new_input = testfun.load_input(sample_input)
         new_data = testfun.generateres(new_input)
         testfun.savefile(new_data, sim_dir, sim_out)
-    else:
-        print("can't resample for selected data")
-        exit(255)
-
-    dims = len(reg_file.dims)
-    name = reg_file.name
-
-    # if 'PNNL' in reg_file.name:
-    if reg_file.name == 'PNNL_7D_mean':
+    elif 'PNNL' in reg_file.name:
         df = pd.DataFrame(reg_file.pts)
         data = df.as_matrix()
         # print(df.columns)
@@ -48,7 +39,12 @@ def sample(spec, data_dir):
         new_data = model.predict(sample_input)
         np.savetxt(Path(sim_dir / sim_out), np.hstack((sample_input, new_data)),
                    delimiter=',')
+    else:
+        print("can't resample for " + reg_file.name)
+        exit(255)
 
+    dims = len(reg_file.dims)
+    name = reg_file.name
 
     reg_file.add_pts_from_csv(sim_dir+'/'+sim_out)
     updated_dataset = reg_file.save_all_pts()
@@ -58,14 +54,7 @@ def sample(spec, data_dir):
     status = subprocess.run(['python', 'post.py', '-d', str(dims), '--name', name, '--p', updated_json])
     linear_fit(updated_json)
 
-    if 'PNNL' in reg_file.name:
-        subprocess.run(['python', 'post.py', '-k', '50', '-b', '1', '-G',
-                        'relaxed beta skeleton', '--name', reg_file.name,
-                        '--all', updated_dataset,'-j', '1', '-t',updated_json],
-                       check=True)
-
     print("New Results are available")
-
     return status.returncode
 
 
@@ -85,8 +74,20 @@ def sample_without_processing(spec, data_dir):
         new_input = testfun.load_input(sample_input)
         new_data = testfun.generateres(new_input)
         testfun.savefile(new_data, sim_dir, sim_out)
+    elif 'PNNL' in reg_file.name:
+        df = pd.DataFrame(reg_file.pts)
+        data = df.as_matrix()
+        # print(df.columns)
+        # print(reg_file.pts[0])
+        print('poop')
+        X = data[:, :-1]
+        y = data[:, -1]
+        model = Predictor(X, y)
+        new_data = model.predict(sample_input)
+        np.savetxt(Path(sim_dir / sim_out), np.hstack((sample_input, new_data)),
+                   delimiter=',')
     else:
-        print("can't resample for selected data")
+        print("can't resample for " + reg_file.name)
     reg_file.add_pts_from_csv(sim_dir + '/' + sim_out)
     updated_dataset = reg_file.save_all_pts()
     updated_json = reg_file.save_json()
