0a04f060538ca859a4866e83f10a06950dd902b4
orpheus92 <dwang0127@gmail.com>
2018-04-09 16:38:36 -0600

    update server code

--- server/server.py
+++ server/server.py
@@ -4,15 +4,17 @@ from bottle import Bottle, run, static_file, post, request
 from pathlib import Path
 import json
 import argparse
-from sample import create_reg, compute_msc, pts2json
-from compute import compute
+
+from regulus.update.update_reg import get_newreg, get_sample
+from regulus.update.update_topo import update_topo
+
 
 p = argparse.ArgumentParser(description='Regulus server')
 p.add_argument('-d', '--data', default=None, help='data directory')
 args = p.parse_args()
 
 data_dir = Path(args.data or os.environ['REGULUS_DATA_DIR'] or '../data')
-print('*** Using data dir:',data_dir)
+print('*** Using data dir:', data_dir)
 
 app = Bottle()
 
@@ -38,7 +40,7 @@ def catalog():
 @app.route('/data/<path:path>')
 def data(path):
     print('req data ', path)
-    filename = path+'.json'
+    filename = path + '.json'
     print('dataset', filename)
     return static_file(filename, root=str(data_dir))
 
@@ -70,15 +72,16 @@ def recompute():
 
 
 @app.post('/request_samples')
-def validate():
-    spec = request.json
-    print('validate request received', spec)
-    reg_file = create_reg(spec, data_dir)
-    if reg_file == 0:
-        return json.dumps("Can't resample provided data")
-    else:
-        return json.dumps(pts2json(reg_file))
+def get_samples():
+    try:
+        spec = request.json
+        print('sample request received', spec)
+        new_samples = get_sample(spec, data_dir)
+        return json.dumps(new_samples)  # json.dumps(pts2json(reg_file))
 
+    except Exception as e:
+        print(e)
+        print("Error, Could not get new samples")
 
 @app.route('/status/<job_id>')
 def status(job_id):
@@ -106,30 +109,33 @@ def new_job():
 def resample_job(job, spec):
     job['status'] = 'running'
     try:
-        reg_file = create_reg(spec, data_dir)
-        code = compute_msc(reg_file)
-        print("code",code)
+
+        code = get_newreg(spec, data_dir)
         with jobs_lock:
             job['status'] = 'done' if code == 0 else 'error'
             job['code'] = code
+        print('job {} done with code:{}'.format(job['id'], code))
+
     except Exception as e:
         print(e)
         print("Error, Job Not Finished")
         code = 1
-        #with jobs_lock:
-        #job['status'] = 'done' if code == 0 else 'error'
         job['code'] = code
-    print('job {} done with code:{}'.format(job['id'], code))
 
 
 def recompute_job(job, spec):
     job['status'] = 'running'
-    code = compute(spec, data_dir)
-    print('job {} done with code:{}'.format(job['id'], code))
-    with jobs_lock:
-        job['status'] = 'done' if code == 0 else 'error'
+    try:
+        code = update_topo(spec, data_dir)
+        print('job {} done with code:{}'.format(job['id'], code))
+        with jobs_lock:
+            job['status'] = 'done' if code == 0 else 'error'
+            job['code'] = code
+
+    except Exception as e:
+        print(e)
+        print("Error, Job Not Finished")
+        code = 1
         job['code'] = code
 
-
 run(app, host='localhost', port=8081, debug=True, reloader=True)
-
